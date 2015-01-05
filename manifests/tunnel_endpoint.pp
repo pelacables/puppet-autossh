f9cd2d88d90cbeb4f8903a277cb518e08f359301
Jason Ball <jball@westpac.com.au>
2015-01-06 09:49:42 +1100

    Added tunnel-endpoint exported resource

--- manifests/tunnel_endpoint.pp
+++ manifests/tunnel_endpoint.pp
@@ -8,15 +8,15 @@ define autossh::tunnel_endpoint(
 )
 {
   $permit="\"localhost:${port}\""
-    file_line{"autossh ${user}_authkey_${port}":
-      path => "/home/${user}/.ssh/authorized_keys",
-      line => "command=echo Unauthorised Access,no-agent-forwarding,no-X11-forwarding,permitopen=${permit} ${pubkey}"
-    }
+#    file_line{"autossh ${user}_authkey_${port}":
+#      path => "/home/${user}/.ssh/authorized_keys",
+#      line => "command=echo Unauthorised Access,no-agent-forwarding,no-X11-forwarding,permitopen=${permit} ${pubkey}"
+#    }
 
-#  concat::fragment{"autossh_${user}_authkey_${host}_${port}":
-#    target  => "/home/${user}/.ssh/authorized_keys",
-#    content => "command=echo Unauthorised Access,no-agent-forwarding,no-X11-forwarding,permitopen=${permit} ${pubkey}",
-#    order   => 10,
-#    tag     => "authkey_fragment_${user}_${host}",
-#  }
+  concat::fragment{"autossh_${user}_authkey_${host}_${port}":
+    target  => "/home/${user}/.ssh/authorized_keys",
+    content => "command=echo Unauthorised Access,no-agent-forwarding,no-X11-forwarding,permitopen=${permit} ${pubkey}",
+    order   => 10,
+    tag     => "authkey_fragment_${user}_${host}",
+  }
 }
