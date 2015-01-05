68d2ac03fb9d0fdbb9a5080d93d0a0cc1e66b096
Jason Ball <jball@westpac.com.au>
2015-01-06 09:55:49 +1100

    Added tunnel-endpoint exported resource

--- manifests/tunnel_endpoint.pp
+++ manifests/tunnel_endpoint.pp
@@ -15,7 +15,7 @@ define autossh::tunnel_endpoint(
 
   concat::fragment{"autossh_${user}_authkey_${host}_${port}":
     target  => "/home/${user}/.ssh/authorized_keys",
-    content => "command=echo Unauthorised Access,no-agent-forwarding,no-X11-forwarding,permitopen=${permit} ${pubkey}",
+    content => template('autossh/endpoint.erb'),
     order   => 10,
     tag     => "authkey_fragment_${user}_${host}",
   }
